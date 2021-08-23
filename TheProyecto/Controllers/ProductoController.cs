504c71ada78e11cf2f05b38ded2b82fc75d9ef6f
giovanny estevan hernandez moreno <giovannyestevan@hotmail.com>
2021-08-23 18:22:53 -0500

    Creacion Proveedor

---
+++ TheProyecto/Controllers/ProductoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheProyecto.Models;
+
+namespace TheProyecto.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto.ToList());
+            }
+                
+        }
+    }
+}
---
+++ TheProyecto/Controllers/ProveedorController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheProyecto.Models;
+
+namespace TheProyecto.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.proveedor.ToList());
+            }
+                
+        }
+
+        //vista crear Proveedor
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        //recibir datos crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findProvider = db.proveedor.Find(id);
+                return View(findProvider);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findProvider = db.proveedor.Find(id);
+                    db.proveedor.Remove(findProvider);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        //vista editar
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor findProvider = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findProvider);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        //recibir datos editar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(proveedor editProvider)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor provider = db.proveedor.Find(editProvider.id);
+
+                    provider.nombre = editProvider.nombre;
+
+                    provider.direccion = editProvider.direccion;
+
+                    provider.telefono = editProvider.telefono;
+
+                    provider.nombre_contacto = editProvider.nombre_contacto;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+
+
+    }
+}
